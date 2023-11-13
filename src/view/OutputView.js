7e9ee92024eac84bf980d20b7b61e17f5ad2a691
koblmom <nayun9191@gamil.com>
2023-11-13 18:57:48 +0900

    fix: 주문정보 오류 수정

--- src/view/OutputView.js
+++ src/view/OutputView.js
@@ -5,8 +5,10 @@ const OutputView = {
   printMenu(orders) {
     Console.print("<주문 메뉴>");
     orders.forEach((order) => {
-      const value = Object.values(order);
-      Console.print(`${value}개`);
+      const values = Object.values(order);
+      const menuName = values[0];
+      const quantity = values[1];
+      Console.print(`${menuName} ${quantity}개`);
     });
   },
   printTotalOrder(TotalOrder) {
