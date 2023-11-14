a5293f0ae40a0a9aef628e42698b0152a4e45f0d
koblmom <nayun9191@gamil.com>
2023-11-15 01:58:44 +0900

    fix: 주석제거

--- src/App.js
+++ src/App.js
@@ -7,18 +7,6 @@ class App {
     this.EventController = new EventController();
   }
   async run() {
-    // let inputValid = false;
-
-    // while (!inputValid) {
-    //   try {
-    //     await this.EventController.handleDate();
-    //     await this.EventController.handleOrder();
-    //     await this.EventController.handleEvent();
-    //     inputValid = true;
-    //   } catch (err) {
-    //     Console.print(err.message);
-    //   }
-    // }
     await this.retry(() => this.EventController.handleDate());
     await this.retry(() => this.EventController.handleOrder());
     await this.retry(() => this.EventController.handleEvent());
@@ -28,7 +16,6 @@ class App {
     try {
       return await callback();
     } catch (err) {
-      // console.error(err);
       Console.print(err.message);
       return this.retry(callback);
     }
